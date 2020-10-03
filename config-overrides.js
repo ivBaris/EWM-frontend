cd1b37f24a1228e09adaa7dc6470eca855aeefd3
unknown <ibarisic@web.de>
2020-10-04 01:23:15 +0200

    update config

--- config-overrides.js
+++ config-overrides.js
@@ -15,12 +15,12 @@ module.exports = {
   },
 };
 
-// function removeSWPrecachePlugin(config) {
-//   const swPrecachePluginIndex = config.plugins.findIndex((element) => {
-//     return element.constructor.name === "SWPrecacheWebpackPlugin";
-//   });
-//   if (swPrecachePluginIndex !== -1) {
-//     config.plugins.splice(swPrecachePluginIndex, 1);
-//   }
-//   return config;
-// }
+function removeSWPrecachePlugin(config) {
+  const swPrecachePluginIndex = config.plugins.findIndex((element) => {
+    return element.constructor.name === "SWPrecacheWebpackPlugin";
+  });
+  if (swPrecachePluginIndex !== -1) {
+    config.plugins.splice(swPrecachePluginIndex, 1);
+  }
+  return config;
+}
