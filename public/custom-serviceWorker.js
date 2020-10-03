56cab661e22805a0d8141a5d248c2d43ae8584e4
unknown <ibarisic@web.de>
2020-10-04 00:39:50 +0200

    update sw

--- public/custom-serviceWorker.js
+++ public/custom-serviceWorker.js
@@ -1,24 +1,28 @@
-importScripts("/precache-manifest.265d4829533261042425f08b8282723b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-import { precacheAndRoute } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate, NetworkOnly } from "workbox-strategies";
-import { BackgroundSyncPlugin } from "workbox-background-sync";
-import process from "process";
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
 
 console.log("XD bin drin");
 
-const backgroundSync = new BackgroundSyncPlugin("addEvent");
+const backgroundSync = new workbox.backgroundSync.BackgroundSyncPlugin(
+  "addEvent"
+);
 
-registerRoute(
+workbox.routing.registerRoute(
   ({ url }) =>
     url.pathname === "https://event-with-me.herokuapp.com/api/events",
-  new NetworkOnly({ plugins: [backgroundSync] }),
+  new workbox.strategies.NetworkOnly({ plugins: [backgroundSync] }),
   "POST"
 );
 
-registerRoute(/\.*$/, new StaleWhileRevalidate({ cacheName: "static" }));
-registerRoute("/", new StaleWhileRevalidate({ cacheName: "static" }));
+workbox.routing.registerRoute(
+  /\.*$/,
+  new StaleWhileRevalidate({ cacheName: "static" })
+);
+workbox.routing.registerRoute(
+  "/",
+  new StaleWhileRevalidate({ cacheName: "static" })
+);
 
 const handlerCb = async ({ url, request, event, params }) => {
   const response = await fetch(request);
@@ -26,7 +30,7 @@ const handlerCb = async ({ url, request, event, params }) => {
   return new Response(`${responseBody} <!-- Look Ma. Added Content. -->`);
 };
 
-registerRoute(
+workbox.routing.registerRoute(
   handlerCb,
   new CacheFirst({
     cacheName: "dynamic",
@@ -34,6 +38,5 @@ registerRoute(
 );
 
 if (process.env.NODE_ENV === "production") {
-  precacheAndRoute(self.__WB_MANIFEST);
+  workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 }
-
