e2a17a9f2164dd3f575a0a230ed738ae7343052f
unknown <ibarisic@web.de>
2020-10-04 00:33:01 +0200

    update config

--- config-overrides.js
+++ config-overrides.js
@@ -1,26 +1,15 @@
-const workboxPlugin = require("workbox-webpack-plugin");
-const path = require("path");
-
-module.exports = {
-  webpack: function (config, env) {
-    if (env === "production") {
-      const workboxConfigProd = {
-        swSrc: path.join(__dirname, "public", "custom-serviceWorker.js"),
-        swDest: "custom-serviceWorker.js",
-      };
-      config = removeSWPrecachePlugin(config);
-      config.plugins.push(new workboxPlugin.InjectManifest(workboxConfigProd));
-    }
-    return config;
-  },
+const { override, addWebpackPlugin } = require("customize-cra");
+const { InjectManifest } = require("workbox-webpack-plugin");
+module.exports = (webpack, ...args) => {
+  // remove GenerateSW plugin
+  webpack.plugins.pop();
+  const overridenConf = override(
+    addWebpackPlugin(
+      new InjectManifest({
+        swSrc: "./public/custom-serviceWorker.js",
+        swDest: "./service-worker.js",
+      })
+    )
+  )(webpack, ...args);
+  return overridenConf;
 };
-
-function removeSWPrecachePlugin(config) {
-  const swPrecachePluginIndex = config.plugins.findIndex((element) => {
-    return element.constructor.name === "SWPrecacheWebpackPlugin";
-  });
-  if (swPrecachePluginIndex !== -1) {
-    config.plugins.splice(swPrecachePluginIndex, 1);
-  }
-  return config;
-}
