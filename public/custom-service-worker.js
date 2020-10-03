ad2122c093c0ad90c3c1535dad6ee7a6bde62b26
unknown <ibarisic@web.de>
2020-10-04 00:55:22 +0200

    test

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -24,18 +24,18 @@ workbox.routing.registerRoute(
   new workbox.strategies.StaleWhileRevalidate({ cacheName: "static" })
 );
 
-const handlerCb = async ({ url, request, event, params }) => {
-  const response = await fetch(request);
-  const responseBody = await response.text();
-  return new Response(`${responseBody} <!-- Look Ma. Added Content. -->`);
-};
+// const handlerCb = async ({ url, request, event, params }) => {
+//   const response = await fetch(request);
+//   const responseBody = await response.text();
+//   return new Response(`${responseBody} <!-- Look Ma. Added Content. -->`);
+// };
 
-workbox.routing.registerRoute(
-  handlerCb,
-  new workbox.strategies.CacheFirst({
-    cacheName: "dynamic",
-  })
-);
+// workbox.routing.registerRoute(
+//   handlerCb,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: "dynamic",
+//   })
+// );
 
 if (process.env.NODE_ENV === "production") {
   workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
