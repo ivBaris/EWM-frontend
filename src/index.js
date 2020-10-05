cb267be0e676003b1448966a39697803d9e52afd
unknown <ibarisic@web.de>
2020-10-05 23:49:17 +0200

    update manifest

--- src/index.js
+++ src/index.js
@@ -40,7 +40,5 @@ ReactDOM.render(
 );
 
 export default theme;
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 registerServiceWorker();
